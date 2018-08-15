4aa90e472f0c798830e81267513d5623d1726d78
hand-of-cthulhu <3rdhandofcthulhu@gmail.com>
2018-08-15 04:30:21 +0100

    Init commit

---
+++ Assets/movement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("YOLO");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		float forwards = Input.GetAxis("Vertical");
+
+		this.transform.position = new Vector3(this.transform.position.x , this.transform.position.y + forwards, this.transform.position.z);
+	}
+}
